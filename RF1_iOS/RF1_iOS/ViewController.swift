37ada6a0426db453e8f035bc1a3fd7af7d147a92
Keegan Jebb <ksjebb@gmail.com>
2018-03-12 16:16:54 -0400

    Initial commit with RF1_iOS project folder

---
+++ RF1_iOS/RF1_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RF1_iOS
+//
+//  Created by Keegan Jebb on 2018-03-12.
+//  Copyright © 2018 Keegan Jebb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
